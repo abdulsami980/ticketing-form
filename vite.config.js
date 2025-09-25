515f42e72adff2b0bfbc698eb4bef8ff22d8fa0f
abdulsami980 <abdulsamiimran786@gmail.com>
2025-09-25 15:20:56 +0500

    YRL FIXED BRUH

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5678,
     proxy: {
       "/n8n": {
-        target: "https://7af7d8dc9fe5.ngrok-free.app",
+        target: "https://c7349e451101.ngrok-free.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/n8n/, ""),
